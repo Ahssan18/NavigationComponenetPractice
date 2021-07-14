0ff429c28cd3202c8ac2a43fa33b0ae5139efed5
Ahssan <ahssanakhtar18@gmail.com>
2021-07-15 02:40:47 +0500

    in this brach i implement multiple back stack

--- app/src/main/java/com/example/navigationcomponenetpractice/Activities/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponenetpractice/Activities/MainActivity.kt
@@ -2,25 +2,41 @@ package com.example.navigationcomponenetpractice.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.navigationcomponenetpractice.R
+import com.example.navigationcomponenetpractice.databinding.ActivityMainBinding
+import com.example.navigationcomponenetpractice.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navcontroller: NavController
+    lateinit var binding:ActivityMainBinding
+     var navcontroller: LiveData<NavController>?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        navcontroller = navHostFragment.navController
-        setupActionBarWithNavController(navcontroller)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       setUpNavigation()
 
     }
 
+    private fun setUpNavigation() {
+        var navgraph_ids=listOf(R.navigation.nav_home,R.navigation.nav_setting,R.navigation.nav_notification)
+        var controller=binding.nav.setupWithNavController(navgraph_ids,supportFragmentManager,R.id.fragment,intent)
+        controller.observe(this){
+            setupActionBarWithNavController(it)
+        }
+        navcontroller=controller
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setUpNavigation()
+    }
     override fun onSupportNavigateUp(): Boolean {
-        return navcontroller.navigateUp() || super.onNavigateUp()
+        return navcontroller?.value?.navigateUp()!! || super.onNavigateUp()
     }
 
 }
